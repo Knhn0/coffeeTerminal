c1c3e61261980bca4b7d345fea002f9215ccb67a
Knhn0 <Knhno@yandex.ru>
2022-11-20 19:50:14 +0300

    Added OrderController

---
+++ CoffeeTerminal/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using CoffeeTerminal.Domain.Entity;
+using CoffeeTerminal.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeTerminal.Controllers;
+
+public class OrderController : BaseController
+{
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpPost("create")]
+    public async Task<ActionResult<bool>> CreateOrder(Order order)
+    {
+        var result = await _orderService.CreateOrder(order);
+        if (result == null)
+        {
+            return BadRequest("result is null");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<int>> GetOrderById (int id)
+    {
+        var order = await _orderService.Get(id);
+        if (id == 0)
+        {
+            return BadRequest($"Id is null");
+        }
+
+        return Ok(order);
+    }
+}
